c76ba75ceca324a18ac57f5cbaa6fd22c9e81070
LixinXu0 <146698372+LixinXu0@users.noreply.github.com>
2023-11-13 23:27:16 +0800

    Update lift_drag_modelling.m

--- Flight/lift_drag_modelling.m
+++ Flight/lift_drag_modelling.m
@@ -7,7 +7,7 @@
 % the coefficient value tange of millitary trainers 
 % https://www.ae.utexas.edu/~varghesep/class/aircraft/Suggestions.p
 %need CL or we can use the maximum CLmax for analysis?
-CLmax = 1.8; 
+CLmax = 1.54; 
 % Maximum lift coefficient normally 1.2-1.8
 CLmaxl = 2.5; 
 % Landing maximum lift coefficient normally 1.6-2.2 fighters 1.6-2.6
@@ -47,10 +47,42 @@ t = c*12/100; % thickness
 x_chamber = c*4/10; % x position of the chamber
 T = 269; %temperature K
 mu = 1.458e-6*(T^1.5/(110.4+T));% Ns/m2 
-Mach = 0.263;
-V_cruise = Mach*340.2900; % cruise speed m/s 
+%Mach = 0.263;
+V_cruise = 77;
+Mach = V_cruise/340.2900;
+%V_cruise = Mach*340.2900; % cruise speed m/s 
 rho = 0.909; %kg/m3
-Re = (rho * V_cruise*c)/mu; %Reynolds numbers
+Re = (rho * 77*c)/mu; %Reynolds numbers
 CDimax = (CLmax^2)/(pi*A*e);
 Cf = Cfe; % skin friction coeeficient, same as equivalent one before
 CDf = Cf*k; % skin friction drag coefficient
+
+
+%%%%% 3D
+
+%CLalpha = 2*pi*A/(2+sqrt(A^2+4));
+%CLmax0 = 2*(W/S)/(rho*(V_cruise^2));
+%cbar = S/b;
+%Clalpha = CLalpha/0.9;
+%Cmalpha = 0.0002*alpha-0.01198;
+%alpha_ZL = -Cl0/Clalpha;
+%CL0 = abs(alpha_ZL)*Clalpha;
+%CM_aw = CLalpha*(Cmalpha/Clalpha);
+
+beta = (1-(Mach^2))^0.5; %Mach number parameter(Prandtl-Glauert)
+Clalpha = 2*pi;
+kappa = 2*pi; %ratio of 2-dimension lift curve slope to 2*pi
+%In general, most airfoils only approximately dispaly the 2*pi lift slope
+%as predicted by thin airfoil theory. That is because airfoils are not
+%actually infinitely thin in practice, and will deviate from thin airfoil
+%theory by a small amount
+LAMBDA_c2 = 0; %sweepback of mid-chord 
+%The resulting expression below is valid for noncurved planform shapes and
+%Mach is not higher than 0.8
+CLalpha = (2*pi*A)/(2+sqrt(((A*beta/kappa)^2)*(1+((tan(LAMBDA_c2)^2)/(beta^2)))+4)); %per rad
+
+W = 1321.88; %5880N = 1321.88Ibf
+W_S = 12.8237; %614N/m2 = 12.8237Ibf/ft2
+S = W/W_S; %ft2
+Vs = 0.952*sqrt(2*W/(rho*S*CLmax)); %Wing loading on stalling speed
+CLmax0 = CLmax*0.9; %Max lift coefficient of wing in its clean configuration
